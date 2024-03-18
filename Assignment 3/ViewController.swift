b7238735c4204d9cb01427d4447d2fd04f374159
Gajjar Harsh <harshgajjar.mscit20@vnsgu.ac.in>
2024-03-18 14:46:32 +0530

    Assignment 3

--- Assignment 3/ViewController.swift
+++ Assignment 3/ViewController.swift
@@ -17,3 +17,19 @@ class ViewController: UIViewController {
 
 }
 
+
+//ractical Assignment 3:
+//1.
+//Develop iOS application to show the relationship between entities:
+//                Stud_master (rollno, sem, div, name, area, city, dob, bgp, gender)
+//                Stud_marks(rollno, div, rdbms, micro, es,wd)
+//
+//2.
+//Develop iOS application to show the relationship between entities:
+//               Book (bookid, bookname, author, price)
+//               BookIssueReturn (bookid, status(issue/return), irdate)
+//
+//3.
+//Develop iOS application to show the relationship between entities:
+//              Assignment (assigno, subject, assigndate, faculty)
+//              Assign_Subm (assigno, sub_status, subdate, grade)
